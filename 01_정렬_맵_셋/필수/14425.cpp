2819df7e5b918648ce69040f56a05d925e887950
Hee Seo Kim <hisounni@ewhain.net>
2024-02-19 22:13:08 +0900

    [01_정렬_맵_셋] 2월 19일

---
+++ 01_정렬_맵_셋/필수/14425.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int N, M;
+    vector<string> str;
+
+    cin >> N >> M;
+
+    for (int i = 0; i < N; i++) {
+        cin >> str[i];
+    }
+
+    for (int i = 0; i < M; i++){   
+        cin >> str[i];
+    }
+
+}
