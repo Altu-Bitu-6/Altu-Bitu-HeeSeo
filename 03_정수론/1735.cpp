9633034fc563ce816f55ca6bac75343c316ca710
Hee Seo Kim <hisounni@ewhain.net>
2024-03-04 16:40:48 +0900

    3/4

--- 03_정수론/1735.cpp
+++ 03_정수론/1735.cpp
@@ -1,1 +1,30 @@
 #include <iostream>
+
+using namespace std;
+
+int gcd(int a, int b) { //유클리드 호제법
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+void solution(int A1, int B1, int A2, int B2) {
+    int n = A1 * B2 + A2 * B1;
+    int m = B1 * B2; //일단 더하고
+
+    int gcdNum = gcd(n, m);
+
+    n /= gcdNum;
+    m /= gcdNum; // 최대공약수로 나눠서 기약분수 만들기
+
+    cout << n << " " << m;
+}
+
+int main() {
+    int A1, B1, A2, B2;
+    cin >> A1 >> B1 >> A2 >> B2;
+
+    solution(A1, B1, A2, B2);
+
+    return 0;
+}
